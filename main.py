81ca1edc5be59160ba42f469eedbada11e5a6e15
pavankalyangojju <95854682+pavankalyangojju@users.noreply.github.com>
2022-11-09 17:39:38 +0530

    creating rotures for api endpoints and adding courses and sections to api

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from typing import Optional,List
 from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 
+from api import users, courses, sections
+
+from api.users import router
+
 app = FastAPI(
     title="FasT-ApI-Prac",
     description="B for managing students and courses.",
@@ -16,25 +20,7 @@ app = FastAPI(
 )
 
 
-users = []
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
 
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
-
-@app.get("/users", response_model=List[User])
-async def get_users():
-    return users
-
-@app.post("/users")
-async def create_user(user:User):
-    users.append(user)
-    return "Success"
-
-@app.get("/users/{id}")
-async def get_user(
-    id: int = Path(..., description="The ID of the user you want to retrieve.",gt=2),
-    q: str = Query(None, max_length=5)
-):
-    return { "user": users[id], "query": q }
